65dc0a6567a10783db311975365e7382d72c6ffd
pjlangley <pjlangley@users.noreply.github.com>
2020-04-27 20:28:19 +0100

    TS and webpack bundling example project

---
+++ webpack-bundle/src/Heading.ts
@@ -0,0 +1,4 @@
+const Heading = document.createElement('h1');
+Heading.innerHTML = "My App";
+
+export { Heading };
---
+++ webpack-bundle/src/index.ts
@@ -0,0 +1,7 @@
+import { Heading } from './Heading';
+
+const App = () => {
+  document.body.appendChild(Heading);
+};
+
+App();
